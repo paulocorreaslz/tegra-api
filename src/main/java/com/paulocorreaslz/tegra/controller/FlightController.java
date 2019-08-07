064e13c3ab56e530e2fc446ea4178f4f7cad8978
Paulo Correa <pauloyaco@gmail.com>
2019-08-06 22:26:56 -0300

    inclusao de biblioteca de leitura de arquivos CSV e criação de metodo de retorno no controller para teste de resposta de API

--- src/main/java/com/paulocorreaslz/tegra/controller/FlightController.java
+++ src/main/java/com/paulocorreaslz/tegra/controller/FlightController.java
@@ -6,10 +6,20 @@ package com.paulocorreaslz.tegra.controller;
 *
 */
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 @CrossOrigin(origins = {"http://localhost","*"})
 @RestController
@@ -20,5 +30,26 @@ public class FlightController {
   public String online() {
 	  return "online";
   }
- 
+  
+  @GetMapping("/uber")
+	public List<CSVRecord> getUberAirFlights()  {
+		List<CSVRecord> dados = null;
+		try {
+			File file = new ClassPathResource("uberair.csv").getFile();
+			System.out.println(file.getAbsolutePath());
+			Reader in = new FileReader( file );
+	        CSVParser parser = new CSVParser( in, CSVFormat.DEFAULT );
+	        List<CSVRecord> list = parser.getRecords();
+	        dados = list;
+	        for( CSVRecord row : list )
+	            for( String entry : row )
+	                System.out.println( entry );
+	        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dados;
+	}
+  
 }
