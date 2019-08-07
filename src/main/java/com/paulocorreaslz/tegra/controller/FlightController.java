21353635add795364683716485f21030e2b099c0
Paulo Correa <pauloyaco@gmail.com>
2019-08-06 23:13:58 -0300

    Inclusao de biblioteca de leitura de Json, criação de metodo para leitura de dados 99planes.json, teste de retorno de API de leitura de dados JSON

--- src/main/java/com/paulocorreaslz/tegra/controller/FlightController.java
+++ src/main/java/com/paulocorreaslz/tegra/controller/FlightController.java
@@ -7,6 +7,7 @@ package com.paulocorreaslz.tegra.controller;
 */
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -17,9 +18,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paulocorreaslz.tegra.model.Voo;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 @CrossOrigin(origins = {"http://localhost","*"})
 @RestController
@@ -52,4 +59,55 @@ public class FlightController {
 		return dados;
 	}
   
+  	@GetMapping("/planes")
+	  public void getPlanesFlights() throws IOException{
+		  
+  		  File file = new ClassPathResource("99planes.json").getFile();
+
+	      JSONParser jsonParser = new JSONParser();
+	      
+	      try (FileReader reader = new FileReader(file))
+	      {
+	          Object obj = jsonParser.parse(reader);
+	
+	          JSONArray flightList = (JSONArray) obj;
+	          //System.out.println(flightList);
+	           
+	          flightList.forEach( flitghsJSon -> transformFlightInfo( (JSONObject) flitghsJSon ) );
+	
+	      } catch (FileNotFoundException e) {
+	          e.printStackTrace();
+	      } catch (IOException e) {
+	          e.printStackTrace();
+	      } catch (ParseException e) {
+	          e.printStackTrace();
+	      }
+	  }
+  	
+	  private void transformFlightInfo(JSONObject info)
+	  {
+	    
+		  String vooJsonValue = (String) info.get("voo");
+	      System.out.println("voo:"+vooJsonValue);
+	     
+	      String origemJsonValue = (String) info.get("origem");
+	      System.out.println("origem:"+origemJsonValue);
+	     
+	      String destinoJsonValue = (String) info.get("destino");
+	      System.out.println("destino:"+destinoJsonValue);
+	     
+	      String dataJsonValue = (String) info.get("data_saida");
+	      System.out.println("data:"+dataJsonValue);
+	      
+	      String saidaJsonValue = (String) info.get("saida");
+	      System.out.println("saida:"+saidaJsonValue);
+	      
+	      String chegadaJsonValue = (String) info.get("chegada");
+	      System.out.println("chegada:"+chegadaJsonValue);
+	      
+	      Double valorJsonValue = (Double) info.get("valor");
+	      System.out.println("data:"+valorJsonValue);
+	      
+	      System.out.println("----------------------------------");
+	  }
 }
