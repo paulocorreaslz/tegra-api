a3cd275745bb0b0243d66d4dd1b3630813737b5f
Paulo Correa <pauloyaco@gmail.com>
2019-09-26 22:12:01 -0300

    heroku config

--- src/main/java/com/paulocorreaslz/tegra/TegraApplication.java
+++ src/main/java/com/paulocorreaslz/tegra/TegraApplication.java
@@ -7,10 +7,20 @@ package com.paulocorreaslz.tegra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class TegraApplication  {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Super Voos API!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TegraApplication.class, args);
 	}
